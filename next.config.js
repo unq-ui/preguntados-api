2d78512b977c8681968f8c44221351cb5ee08662
Juan Pablo Mottesi <jmottesi@portfoliopersonal.com>
2022-11-19 09:14:13 -0300

    fix: add headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
